d8cfee89a27d83d5d24eb3a8caf8b2e3efef1b82
Rui Teixeira <ruiteixeira@mailbox.org>
2025-11-24 16:45:51 +0000

    updated example (not tested); added github packages remote

--- examples/sample-app/src/main/java/io/github/ruitx/Repository.java
+++ examples/sample-app/src/main/java/io/github/ruitx/Repository.java
@@ -1,8 +1,12 @@
 package io.github.ruitx;
 
+import static io.github.ruitx.beans.DatabaseFactory.DEFAULT_DB;
+
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import org.nexus.NexusDatabase;
+import org.nexus.exceptions.DatabaseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,28 +15,33 @@ public class Repository {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Repository.class);
 
-  private final NexusDatabase db;
+  private final NexusDatabase db1;
 
   @Inject
-  public Repository(NexusDatabase db) {
-    this.db = db;
+  public Repository(@Named(DEFAULT_DB) NexusDatabase db1) {
+    this.db1 = db1;
   }
 
   public Integer getSample(String name) {
-    return db.query(
-        "SELECT COUNT(*) FROM test t WHERE t.name = ?",
-        rs -> rs.getInt(1),
-        name
-    ).getFirst();
+    try {
+      return db1.query(
+          "SELECT COUNT(*) FROM test t WHERE t.name = ?",
+          rs -> rs.getInt(1),
+          name
+      ).getFirst();
+
+    } catch (DatabaseException e) {
+      LOGGER.error(e.getMessage(), e);
+      return null;
+    }
+
   }
 
   public Boolean postSample(String name) {
-    return db.insert(
+    return db1.insert(
         "INSERT INTO test (name) VALUES (?)",
         _ -> true,
         name
     );
   }
 }
-
-
--- examples/sample-app/src/main/java/io/github/ruitx/beans/DatabaseFactory.java
+++ examples/sample-app/src/main/java/io/github/ruitx/beans/DatabaseFactory.java
@@ -1,14 +1,25 @@
 package io.github.ruitx.beans;
 
 import io.avaje.inject.Bean;
+import io.avaje.inject.External;
 import io.avaje.inject.Factory;
+import jakarta.inject.Named;
 import org.nexus.NexusDatabase;
+import org.nexus.NexusDatabaseRegistry;
 
 @Factory
 public class DatabaseFactory {
 
+  public static final String DEFAULT_DB = "db1";
+
   @Bean
-  public NexusDatabase defaultDatabase(DatabaseRegistry registry) {
+  @Named(DEFAULT_DB)
+  NexusDatabase db1(@External NexusDatabaseRegistry registry) {
+    return registry.get(DEFAULT_DB);
+  }
+
+  @Bean
+  NexusDatabase defaultDb(@External NexusDatabaseRegistry registry) {
     return registry.getDefault();
   }
 }
