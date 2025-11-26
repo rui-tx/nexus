6a5a83b3162114126640c02b14913c3f2ea31473
Rui Teixeira <ruiteixeira@mailbox.org>
2025-11-26 15:10:18 +0000

    Update docs

---
+++ docs/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Nexus Documentation",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/nexus/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Nexus Documentation",
+    "uri": "/nexus/index.html"
+  },
+  {
+    "breadcrumb": "Nexus Documentation",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/nexus/tags/index.html"
+  }
+]
